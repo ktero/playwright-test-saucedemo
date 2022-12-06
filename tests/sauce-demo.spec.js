156cb80a7fc66c0126fd4f88fe53b54511f770f9
Kenneth Tero <ktero.dev@gmail.com>
2022-12-06 21:39:14 +0800

    Added new function checkIfItemsExistInList instead of hardcoding the code of checking the item if it is in the list in every test and also improve test names

--- tests/sauce-demo.spec.js
+++ tests/sauce-demo.spec.js
@@ -32,7 +32,7 @@ test.describe('Checkout items', () => {
     const checkoutLastName  = 'Doe'
     const postalCode        = '1000';
 
-    test('Checkout one item', async ({ page }) => {
+    test('Test 1: Checkout one item', async ({ page }) => {
 
         const ITEM = [ITEMS[0]];
         addItemsToCart(page, ITEM);
@@ -42,7 +42,7 @@ test.describe('Checkout items', () => {
         await expect(page).toHaveURL(/.*cart.html/);
         await expect(page.locator('.title')).toHaveText('Your Cart');
         await expect(page.locator('.cart_item')).toHaveCount(1);
-        await expect(page.locator('.inventory_item_name')).toHaveText(ITEM);
+        checkIfItemsExistInList(page, ITEM);
         await page.locator('#checkout').click();
         
         await expect(page).toHaveURL(/.*checkout-step-one.html/);
@@ -55,7 +55,7 @@ test.describe('Checkout items', () => {
         await expect(page).toHaveURL(/.*checkout-step-two.html/);
         await expect(page.locator('.title')).toHaveText('Checkout: Overview');
         await expect(page.locator('.cart_item')).toHaveCount(1);
-        await expect(page.locator('.inventory_item_name')).toHaveText(ITEM);
+        checkIfItemsExistInList(page, ITEM);
         await page.locator('#finish').click();
 
         await expect(page).toHaveURL(/.*checkout-complete.html/);
@@ -67,7 +67,7 @@ test.describe('Checkout items', () => {
         await expect(page.locator('.title')).toHaveText('Products');
     });
 
-    test('Checkout multiple items', async ({ page }) => {
+    test('Test 2: Checkout multiple items', async ({ page }) => {
 
         // Add the 3 items to cart
         addItemsToCart(page, ITEMS);
@@ -78,9 +78,7 @@ test.describe('Checkout items', () => {
         await expect(page.locator('.title')).toHaveText('Your Cart');
         await expect(page.locator('.cart_item')).toHaveCount(3);
 
-        for(const item of ITEMS) {
-            await expect(page.locator('.inventory_item_name').filter({ hasText: item })).toHaveText(item);
-        }
+        checkIfItemsExistInList(page, ITEMS);
         await page.locator('#checkout').click();
 
         await expect(page).toHaveURL(/.*checkout-step-one.html/);
@@ -93,9 +91,7 @@ test.describe('Checkout items', () => {
         await expect(page).toHaveURL(/.*checkout-step-two.html/);
         await expect(page.locator('.title')).toHaveText('Checkout: Overview');
         await expect(page.locator('.cart_item')).toHaveCount(3);
-        for(const item of ITEMS) {
-            await expect(page.locator('.inventory_item_name').filter({ hasText: item })).toHaveText(item);
-        }
+        checkIfItemsExistInList(page, ITEMS);
         await page.locator('#finish').click();
 
         await expect(page).toHaveURL(/.*checkout-complete.html/);
@@ -107,7 +103,7 @@ test.describe('Checkout items', () => {
         await expect(page.locator('.title')).toHaveText('Products');
     });
 
-    test('Add 3 items to cart but only checkout 2', async ({ page }) => {
+    test('Test 3: Add 3 items to cart but only checkout 2', async ({ page }) => {
 
         // Add the 3 items to cart
         addItemsToCart(page, ITEMS);
@@ -117,18 +113,14 @@ test.describe('Checkout items', () => {
         await expect(page).toHaveURL(/.*cart.html/);
         await expect(page.locator('.title')).toHaveText('Your Cart');
         await expect(page.locator('.cart_item')).toHaveCount(3);
-        for(const item of ITEMS) {
-            await expect(page.locator('.inventory_item_name').filter({ hasText: item })).toHaveText(item);
-        }
+        checkIfItemsExistInList(page, ITEMS);
 
         // Remove second item in the array
         await page.locator('.cart_item').filter({ hasText: ITEMS[1] }).getByRole('button', { name: 'Remove' }).click();
         await expect(page.locator('.cart_item')).toHaveCount(2);
 
         const modified_items = ITEMS.filter((e) => { return e != ITEMS[1] });
-        for(const item of modified_items) {
-            await expect(page.locator('.inventory_item_name').filter({ hasText: item })).toHaveText(item);
-        }
+        checkIfItemsExistInList(page, modified_items);
         await page.locator('#checkout').click();
 
         await expect(page).toHaveURL(/.*checkout-step-one.html/);
@@ -141,9 +133,7 @@ test.describe('Checkout items', () => {
         await expect(page).toHaveURL(/.*checkout-step-two.html/);
         await expect(page.locator('.title')).toHaveText('Checkout: Overview');
         await expect(page.locator('.cart_item')).toHaveCount(2);
-        for(const item of modified_items) {
-            await expect(page.locator('.inventory_item_name').filter({ hasText: item })).toHaveText(item);
-        }
+        checkIfItemsExistInList(page, modified_items);
         await page.locator('#finish').click();
 
         await expect(page).toHaveURL(/.*checkout-complete.html/);
@@ -159,5 +149,11 @@ test.describe('Checkout items', () => {
 async function addItemsToCart(page, ITEMS_TO_ADD) {
     for(const item of ITEMS_TO_ADD) {
         await page.locator('.inventory_item').filter({ hasText: item }).getByRole('button', { name: 'Add to cart' }).click();
+    }
+}
+
+async function checkIfItemsExistInList(page, ITEMS_TO_CHECK) {
+    for(const item of ITEMS_TO_CHECK) {
+        await expect(page.locator('.inventory_item_name').filter({ hasText: item })).toHaveText(item);
     }
 }
